3eb9bb035bb74e99fcf4f57a39b6a1ef02bb74e3
Paul Greenham <paul.greenham@gmail.com>
2019-04-14 12:32:38 +0300

    slight modification to router paths

--- dist/TempManager.js
+++ dist/TempManager.js
@@ -4,13 +4,13 @@ class TempManager {
     }
 
     async getDataFromDB () {
-        let cities = await $.get('./cities')
+        let cities = await $.get('/cities')
         cities.forEach(c => c.saved = true)
         this._cityData.push(...cities)
     }
 
     async getCityData (cityName) {
-        let cityData = await $.get(`./city/${cityName}`)
+        let cityData = await $.get(`/city/${cityName}`)
         this._cityData.push({
             name: cityData.location.name,
             updatedAt: moment(cityData.current.last_updated, "YYYY-MM-DD HH:mm").format(),
@@ -24,7 +24,7 @@ class TempManager {
     saveCity (cityName) {
         let index = this._cityData.findIndex(c => c.name == cityName)
         this._cityData[index].saved = true
-        $.post('./city', this._cityData[index], function () {})
+        $.post('/city', this._cityData[index], function () {})
     }
 
     removeCity (cityName) {
@@ -32,7 +32,7 @@ class TempManager {
         this._cityData.splice(removeIndex, 1)
         $.ajax({
             type: "DELETE",
-            url: `./city/${cityName}`,
+            url: `/city/${cityName}`,
             success: function () {}
         });
     }
